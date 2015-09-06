e7193fbdd31a3d3aab18ea0619aabb8e6cf9e50a
olegosipenko <oleg.s.osipenko@gmail.com>
2015-09-07 01:39:59 +0300

    added Mortar & Flow support

--- app/src/main/java/oleg/osipenko/mai/App.java
+++ app/src/main/java/oleg/osipenko/mai/App.java
@@ -3,6 +3,8 @@ package oleg.osipenko.mai;
 import android.app.Application;
 import android.content.Context;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.squareup.leakcanary.LeakCanary;
 
 import javax.inject.Singleton;
@@ -10,20 +12,38 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.ObjectGraph;
 import dagger.Provides;
+import flow.StateParceler;
+import mortar.MortarScope;
+import mortar.dagger1support.ObjectGraphService;
 import oleg.osipenko.mai.presentation.MainActivity;
 import oleg.osipenko.mai.presentation.PresentationModule;
+import oleg.osipenko.mai.presentation.mf_boilerplate.GsonParceler;
 
 /**
  * Created by olegosipenko on 06.09.15.
  */
 public class App extends Application {
 
+    private MortarScope rootScope;
+
     @Override
     public void onCreate() {
         super.onCreate();
         LeakCanary.install(this);
-        ObjectGraph.create(new AppModule(this));
     }
+
+    @Override
+    public Object getSystemService(String name) {
+        if (null == rootScope) {
+            rootScope = MortarScope.buildRootScope()
+                    .withService(ObjectGraphService.SERVICE_NAME, ObjectGraph.create(new AppModule(this)))
+                    .build("Root");
+        }
+
+        if (rootScope.hasService(name)) return rootScope.getService(name);
+        return super.getSystemService(name);
+    }
+
 
     @Module(
             includes = PresentationModule.class,
@@ -41,6 +61,18 @@ public class App extends Application {
         @Singleton
         Context providesContext() {
             return context;
+        }
+
+        @Provides
+        @Singleton
+        Gson provideGson() {
+            return new GsonBuilder().create();
+        }
+
+        @Provides
+        @Singleton
+        StateParceler provideParceler(Gson gson) {
+            return new GsonParceler(gson);
         }
     }
 }
--- app/src/main/java/oleg/osipenko/mai/presentation/MainActivity.java
+++ app/src/main/java/oleg/osipenko/mai/presentation/MainActivity.java
@@ -1,34 +1,74 @@
 package oleg.osipenko.mai.presentation;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
+
+import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import flow.Flow;
+import flow.FlowDelegate;
+import flow.History;
+import mortar.MortarScope;
+import mortar.bundler.BundleServiceRunner;
+import mortar.dagger1support.ObjectGraphService;
 import oleg.osipenko.mai.R;
+import oleg.osipenko.mai.presentation.mf_boilerplate.GsonParceler;
+import oleg.osipenko.mai.presentation.mf_boilerplate.HandlesBack;
+import oleg.osipenko.mai.presentation.mf_boilerplate.MortarScreenSwitcherFrame;
+import oleg.osipenko.mai.presentation.screens.FacultiesScreen;
 
-public class MainActivity extends AppCompatActivity {
+import static mortar.bundler.BundleServiceRunner.getBundleServiceRunner;
+
+public class MainActivity extends Activity implements Flow.Dispatcher {
 
     @Bind(R.id.drawer)
     DrawerLayout drawerLayout;
     @Bind(R.id.toolbar)
     Toolbar toolbar;
     @Bind(R.id.container)
-    FrameLayout container;
+    MortarScreenSwitcherFrame container;
     @Bind(R.id.nav_menu)
     NavigationView menu;
+
+    @Inject
+    GsonParceler parceler;
+
+    private MortarScope activityScope;
+    private HandlesBack containerAsHandlesBack;
+    private FlowDelegate flowDelegate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        @SuppressWarnings("deprecation") FlowDelegate.NonConfigurationInstance nonConfig =
+                (FlowDelegate.NonConfigurationInstance) getLastNonConfigurationInstance();
+
+        MortarScope parentScope = MortarScope.getScope(getApplication());
+        String scopeName = getLocalClassName() + "-task-" + getTaskId();
+
+        activityScope = parentScope.findChild(scopeName);
+        if (null == activityScope) {
+            activityScope = parentScope.buildChild()
+                    .withService(BundleServiceRunner.SERVICE_NAME, new BundleServiceRunner())
+                    .build(scopeName);
+        }
+        ObjectGraphService.inject(this, this);
+
+        getBundleServiceRunner(activityScope).onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
+
         menu.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
@@ -40,5 +80,92 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        containerAsHandlesBack = (HandlesBack) container;
+        flowDelegate = FlowDelegate.onCreate(
+                nonConfig,
+                getIntent(),
+                savedInstanceState,
+                parceler,
+                History.single(new FacultiesScreen()),
+                this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        flowDelegate.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        flowDelegate.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        flowDelegate.onPause();
+    }
+
+    @SuppressWarnings("deprecation") // https://code.google.com/p/android/issues/detail?id=151346
+    @Override
+    public Object onRetainNonConfigurationInstance() {
+        return flowDelegate.onRetainNonConfigurationInstance();
+    }
+
+    @Override
+    public Object getSystemService(String name) {
+        if (null != flowDelegate) {
+            Object flowService = flowDelegate.getSystemService(name);
+            if (null != flowService) return flowService;
+        }
+        return activityScope != null && activityScope.hasService(name) ? activityScope.getService(name)
+                : super.getSystemService(name);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        flowDelegate.onSaveInstanceState(outState);
+        getBundleServiceRunner(this).onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!containerAsHandlesBack.onBackPressed()) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            return containerAsHandlesBack.onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        // activityScope may be null in case isWrongInstance() returned true in onCreate()
+        if (isFinishing() && activityScope != null) {
+            activityScope.destroy();
+            activityScope = null;
+        }
+
+        super.onDestroy();
+    }
+
+    @Override
+    public void dispatch(Flow.Traversal traversal, Flow.TraversalCallback callback) {
+        container.dispatch(traversal, callback);
     }
 }
